9cfc79b9f19249d579258eb2d708a6f851189ea4
Marios Kanellopoulos <mariosknl@outlook.com>
2023-02-06 13:41:17 +0200

    initial commit

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'selector-name',
+  template: `
+    <div class="well hoverwell thumbnail">
+      <h2>{{ event.name }}</h2>
+      <div>Date: {{ event.date }}</div>
+      <div>Time: {{ event.time }}</div>
+      <div>Price: \${{ event.price }}</div>
+      <div>
+        <span>Location: {{ event.location.address }}</span>
+        <span>&nbsp;</span>
+        <span>{{ event.location.city }}, {{ event.location.country }}</span>
+      </div>
+    </div>
+  `,
+})
+export class NameComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
