3286191958bdb3f94b23c6169c56457b795a2b93
mariosknl <mariosknl@outlook.com>
2023-02-06 17:22:47 +0200

    adding a site header

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     <div>
       <h1>Upcoming Angular Event</h1>
       <hr />
-      <event-thumbnail [event]="event1"></event-thumbnail>
+      <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
     </div>
   `,
 })
