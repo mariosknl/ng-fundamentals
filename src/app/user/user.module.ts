915a2939562953afbfb9cf989a42ad1859162504
mariosknl <mariosknl@outlook.com>
2023-02-07 12:55:13 +0200

    reactive form with validations added

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login.component';
@@ -8,7 +8,12 @@ import { ProfileComponent } from './profile.component';
 import { userRoutes } from './user.routes';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(userRoutes), FormsModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(userRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   declarations: [ProfileComponent, LoginComponent],
   providers: [],
 })
