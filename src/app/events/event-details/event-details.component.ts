f53fd7bc77589b67faa3c0ee513976930fc41d2a
mariosknl <mariosknl@outlook.com>
2023-02-07 14:55:27 +0200

    padding data out of a child component

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IEvent } from '../shared';
+import { IEvent, ISession } from '../shared';
 import { EventService } from '../shared/event.service';
 
 @Component({
@@ -14,11 +14,15 @@ import { EventService } from '../shared/event.service';
       .event-image {
         height: 100px;
       }
+      a {
+        cursor: pointer;
+      }
     `,
   ],
 })
 export class EventDetailsComponent implements OnInit {
-  event: IEvent = {} as IEvent;
+  event: any = {} as IEvent;
+  addMode: boolean = false;
 
   constructor(
     private eventService: EventService,
@@ -27,5 +31,24 @@ export class EventDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
+  }
+
+  addSession(): void {
+    this.addMode = true;
+  }
+
+  saveNewSession(session: any) {
+    const nextId = Math.max.apply(
+      null,
+      this.event.sessions.map((s: any) => s.id)
+    );
+    session.id = nextId + 1;
+    this.event.sessions.push(session);
+    this.eventService.updateEvent(this.event);
+    this.addMode = false;
+  }
+
+  cancelAddSession(): void {
+    this.addMode = false;
   }
 }
